ca58b4fca922a24881fc801ab8e5e19810f9ced8
Alicewish <anywaywillgo@gmail.com>
2019-05-30 21:24:07 -0400

    info

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
+  precacheUrls.push('/post/other/comic-book-lettering-comicraft-way/');
+
   precacheUrls.push('/post/other/batman-v-superman-injustice/');
 
   precacheUrls.push('/post/other/tim-seeley-on-weibo/');
 
-  precacheUrls.push('/post/other/comic-book-lettering-comicraft-way/');
-
   precacheUrls.push('/post/other/comic-profits-sales/');
 
-  precacheUrls.push('/post/other/flypy/');
+  precacheUrls.push('/post/other/qq-group-announcement-translation-tool/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
